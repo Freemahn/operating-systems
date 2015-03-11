a54a9cc00c74ca621c883d5c44040a7da6bf9505
Freemahn <gordon.pav@gmail.com>
2015-03-11 05:02:26 +0300

    changed structure from /hw1/*** to /***

---
+++ cat/cat.c
@@ -0,0 +1,30 @@
+
+#include <unistd.h>
+#include <string.h>
+#include "helpers.h"
+
+int main(int argc, char* argv[]) {
+    char buf[4096];
+    char* error = strerror(errno);
+    ssize_t read_cur = 0;
+    ssize_t write_cur = 0;
+
+    while(1) {
+        read_cur = read_(STDIN_FILENO, buf, sizeof(buf));
+        if(read_cur == -1) {
+            write_(STDERR_FILENO, error, strlen(error) * sizeof(char));
+            return 1;
+        }
+
+        write_cur = write_(STDOUT_FILENO, buf, read_cur);
+        if(write_cur == -1) {
+            write_(STDERR_FILENO, error, strlen(error) * sizeof(char));
+            return 2;
+        }
+        
+        if(read_cur < sizeof(buf)) {
+            return 0;
+        }
+    }
+    return 0;
+}
---
+++ lib/helpers.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+ssize_t read_(int fd, void *buf, size_t count){
+	int read_cur = 0;
+    int read_all = 0;
+    while(1) {
+        read_cur = read(fd, buf, count);
+        read_all += read_cur;
+        buf += read_cur;
+        count -= read_cur;
+        if(read_cur == count ||read_cur == 0) {
+            return read_all;
+        }
+
+        if(read_cur == -1) {
+            return -1;
+        }
+        
+        
+    }
+}
+ssize_t write_(int fd, const void *buf, size_t count){
+
+	int write_cur = 0;
+    int write_all = 0;
+    while(1) {
+        write_cur = write(fd, buf, count);
+		write_all += write_cur;
+        buf += write_cur;
+        count -= write_cur;
+        if(write_cur == count) {
+            return write_all;
+        }
+
+        if(write_cur == -1) {
+            return -1;
+        }
+
+        
+    }
+	
+}
